1c196e73f8f8456431389fd7cf28c50ec3042f0e
Yatharth Saluja <143877530+Yathur-11@users.noreply.github.com>
2025-04-13 16:07:31 +0000

    f

---
+++ 03_basics.js/03_scope.js
@@ -0,0 +1,24 @@
+if(true){
+    let a = 10
+    var b = 20
+    const c = 30 
+}
+
+// console.log(a);  <--- error
+// console.log(b);  <--- prints 20
+// console.log(c);  <--- error
+
+let aa = 100
+let bb = 200
+
+if(true){
+    let aa=5000
+    bb = 5000
+}
+
+console.log(aa); //<-- prints 100
+console.log(bb); //<-- prints 5000
+
+
+
+
