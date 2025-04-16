cf2c2021fc0f83a0bebff4dd972ac121e9aad751
Yatharth Saluja <143877530+Yathur-11@users.noreply.github.com>
2025-04-16 08:51:23 +0000

    ss

---
+++ 04_iterations.js/four.js
@@ -0,0 +1,19 @@
+const coding = ["Js", "C++", "C", "Java"]
+
+
+
+// three different ways 
+
+// coding.forEach(fn)
+
+// function fn(x) {
+    //     console.log(x)
+// }
+
+// coding.forEach(function(val){
+//     console.log(val);
+// })
+
+// coding.forEach( (val) => {
+//     console.log(val);
+// })
--- 04_iterations.js/three.js
+++ 04_iterations.js/three.js
@@ -7,16 +7,27 @@ const obj = {
     swift: "Swift by apple"
 }
 
-for(const key in obj){
-    console.log(key);
-    console.log(obj[key]);
-}
+// for(const key in obj){
+//     console.log(key);
+//     console.log(obj[key]);
+// }
 
 // using for in loop for arrays
 
 const arr1 = ['hello', 'how', 'are', 'you', 'doing']
 
-for(const key in arr1){
-    console.log(key);            // 0,1,2,3,4
-    console.log(arr1[key]);
+// for(const key in arr1){
+//     console.log(key);            // 0,1,2,3,4
+//     console.log(arr1[key]);
+// }
+
+const map = new Map()
+
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('FR', "France")
+map.set('IR', "Ireland")
+
+for(const key in map){
+    console.log(key)      //no output, because maps are not iterable and if something is not iterable , then we can't write it in this way of loops
 }
